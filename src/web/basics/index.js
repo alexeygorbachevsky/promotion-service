adf05fe2c0a6c504a4a0295239712c47463c9dec
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2022-04-03 15:13:47 +0300

    fix: correct slpit on chunks - delete babel import node, fix banner horizontal scroll, fix height of last news wrapper

--- src/web/basics/index.js
+++
@@ -1,5 +0,0 @@
-export { Button, BlankButton } from "./buttons";
-export { Link } from "./links";
-export { BlankInput, Input } from "./inputs";
-export { Modal } from "./modal";
-export { RadioButton, radioButtonConstants } from "./radio-buttons";
